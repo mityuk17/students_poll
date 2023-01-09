6b24d231e9963efc6ced89185f926ea5efe77301
mityuk17 <mityukov.17@gmail.com>
2023-01-09 14:48:54 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import asyncio
+import datetime
 import logging
+import time
+
 import gspread
 from pyrogram import Client, filters, types
 from questions import questions, answer_variant
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 logging.basicConfig(level=logging.INFO)
 api_id = 9411854
 api_hash = '499c76606cefdeadd4b1ece84a5a9932'
@@ -12,15 +15,82 @@ gc = gspread.service_account('credentials.json')
 email = 'proektnya.pochta@gmail.com'
 table_url = 'https://docs.google.com/spreadsheets/d/1PWRXWUcxjPZHiw6BBm4dJrUR_Mk5gCIp2bJDPXN5IQI/edit#gid=376448824'
 sh = gc.open_by_url(table_url)
+scheduler = AsyncIOScheduler()
 def get_answer_kb(chat_id):
     answer_kb = types.InlineKeyboardMarkup(inline_keyboard=[
-    [types.InlineKeyboardButton(text=answer_variant[0], callback_data=f'answer-0-{chat_id}')],
-    [types.InlineKeyboardButton(text=answer_variant[1], callback_data=f'answer-1-{chat_id}')],
-    [types.InlineKeyboardButton(text=answer_variant[2], callback_data=f'answer-2-{chat_id}')],
-    [types.InlineKeyboardButton(text=answer_variant[3], callback_data=f'answer-3-{chat_id}')],
-    [types.InlineKeyboardButton(text=answer_variant[4], callback_data=f'answer-4-{chat_id}')],
+    [types.InlineKeyboardButton(text=answer_variant[0], callback_data=f'answer=0={chat_id}')],
+    [types.InlineKeyboardButton(text=answer_variant[1], callback_data=f'answer=1={chat_id}')],
+    [types.InlineKeyboardButton(text=answer_variant[2], callback_data=f'answer=2={chat_id}')],
+    [types.InlineKeyboardButton(text=answer_variant[3], callback_data=f'answer=3={chat_id}')],
+    [types.InlineKeyboardButton(text=answer_variant[4], callback_data=f'answer=4={chat_id}')],
     ])
     return answer_kb
+def sheet_expired(chat_id):
+    sh = gc.open_by_url(table_url)
+    worksheet = sh.worksheet(title=chat_id)
+    if worksheet.cell(row=2, col=4).value == 0:
+        return True
+    make_conclusion(chat_id)
+async def start_polls():
+    sh = gc.open_by_url(table_url)
+    worksheet_list = sh.worksheets()
+    for sheet in worksheet_list:
+        if sheet.title == 'example':
+            continue
+        worksheet = sh.worksheet(sheet.title)
+        if worksheet.cell(row=2, col=4).value == 1:
+            continue
+        deadline = int(worksheet.cell(row=2,col=2).value)
+        author_id = worksheet.cell(row=2,col=1).value
+        chat_id = worksheet.title
+        chat_name = worksheet.cell(row=2,col=3).value
+        chat_members = list()
+        sh.del_worksheet(worksheet)
+        async for member in app.get_chat_members(int(chat_id)):
+            chat_members.append([member.user.username, member.user.id])
+        create_sheet_poll(sheet_name=chat_id, deadline=deadline, author_id=author_id,
+                          members=chat_members, chat_name=chat_name)
+        scheduler.add_job(sheet_expired, args=[chat_id], trigger='date',
+                          run_date=datetime.datetime.now() + datetime.timedelta(hours=int(deadline)))
+
+def check_full_done_sheets():
+    sh = gc.open_by_url(table_url)
+    worksheet_list = sh.worksheets()
+    for sheet in worksheet_list:
+        if sheet.title == 'example':
+            continue
+        worksheet = sh.worksheet(sheet.title)
+        if worksheet.cell(row=2,col=4).value == 0:
+            continue
+        users = worksheet.col_values(2)[3:]
+        flag = True
+        for user in users:
+            if get_user_current_question(chat_id=sheet.title, user_id=user):
+                flag = False
+        if flag:
+            make_conclusion(chat_id = sheet.title)
+def make_conclusion(chat_id):
+    sh = gc.open_by_url()
+    worksheet = sh.worksheet(chat_id)
+    users_num = len(worksheet.col_values(2)-3)
+    for i in range(users_num):
+        row = worksheet.row_values(i+4)
+        num_questions = get_user_current_question(chat_id, user_id=row[1])
+        if num_questions == None:
+            Y = sum([i for i in row[2:12] if i])/10
+        elif num_questions == 1:
+            Y = 0
+        else:
+            Y = sum([i for i in row[2:12] if i])/(num_questions-1)
+        worksheet.update_cell(row = i+4,col=13, value=Y)
+        if Y >= 3:
+            udo = 'высокая удовлетворённость'
+        elif Y >= 2 and Y < 3:
+            udo = 'средняя удовлетворённость'
+        else:
+            udo = 'низкая удовлетворённость'
+        worksheet.update_cell(row = i+4,col= 14, value=udo)
+    change_poll_status(chat_id,0)
 def check_existing_sheet(sheet_name):
     sh = gc.open_by_url(table_url)
     worksheet_list = sh.worksheets()
@@ -42,19 +112,17 @@ def change_poll_status(chat_id,status):
 async def write_answer(chat_id,user_id, question_num,answer):
     sh = gc.open_by_url(table_url)
     worksheet = sh.worksheet(str(chat_id))
-    cell = worksheet.find(user_id)
+    cell = worksheet.find(str(user_id))
     worksheet.update_cell(row = cell.row, col=question_num+2, value=answer)
 def get_user_current_question(chat_id,user_id):
     sh = gc.open_by_url(table_url)
     worksheet = sh.worksheet(str(chat_id))
-    cell = worksheet.find(user_id)
+    cell = worksheet.find(str(user_id))
     row_number = cell.row
     values_list = worksheet.row_values(row_number)
     next_question = None
-    for i in range(1 , 11):
-        if not (values_list[ i + 2 ]):
-            next_question = i
-            break
+    if len(values_list) < 12:
+        next_question = len(values_list)-1
     return next_question
 def get_users_for_notification():
     users = list()
@@ -78,14 +146,14 @@ async def notificate_users():
 async def send_poll_notification(chat_id, user_id):
 
     chat = await app.get_chat(chat_id)
-    kb = types.InlineKeyboardMarkup(inline_keyboard=[[types.InlineKeyboardButton(text='Перейти к прохождению опроса', callback_data=f'get_question-{str(chat_id)}')]])
+    kb = types.InlineKeyboardMarkup(inline_keyboard=[[types.InlineKeyboardButton(text='Перейти к прохождению опроса', callback_data=f'get_question={str(chat_id)}')]])
 
     await app.send_message(chat_id=user_id, text=f'В группе {chat.title} проводится опрос', reply_markup=kb)
 
 @app.on_callback_query()
 async def get_query(client: Client, callback_query: types.CallbackQuery):
-    if callback_query.data.startswith('get_question-'):
-        chat_id = int(callback_query.data.split('-')[-1])
+    if callback_query.data.startswith('get_question='):
+        chat_id = int(callback_query.data.split('=')[-1])
         user_id = callback_query.message.chat.id
         question = get_user_current_question(chat_id,user_id)
         await app.delete_messages(chat_id=user_id, message_ids=callback_query.message.id)
@@ -94,8 +162,8 @@ async def get_query(client: Client, callback_query: types.CallbackQuery):
             return True
         answer_kb =get_answer_kb(chat_id)
         await app.send_message(chat_id=user_id,text=questions[question-1], reply_markup=answer_kb)
-    elif callback_query.data.startswith('answer-'):
-        chat_id = int(callback_query.data.split('-')[-1])
+    elif callback_query.data.startswith('answer='):
+        chat_id = int(callback_query.data.split('=')[-1])
         user_id = callback_query.message.chat.id
         answer_variant =int(callback_query.data.split('-')[1])
         question_num = get_user_current_question(chat_id=chat_id, user_id=user_id)
@@ -119,13 +187,19 @@ async def setup(client : Client, message: types.Message):
         if len(message.text.split()) == 2:
             deadline = message.text.split()[-1]
             if deadline.isdigit():
-                await message.reply(f'Успешно было запущено проведение опросов в этой группе. Время проведения опросов(в часах): {message.text.split()[-1]}')
+
                 chat_members = list()
                 async for member in app.get_chat_members(message.chat.id):
                     chat_members.append([member.user.username, member.user.id])
                 create_sheet_poll(sheet_name=message.chat.id, deadline = deadline, author_id= message.from_user.id, members= chat_members, chat_name= message.chat.title)
+                scheduler.add_job(sheet_expired, args=[message.chat.id], trigger='date', run_date =datetime.datetime.now()+datetime.timedelta(hours=int(deadline)))
+                await message.reply(f'Успешно было запущено проведение опросов в этой группе. Время проведения опросов(в часах): {message.text.split()[-1]}')
             else:
                 await message.reply('Введённый параметр не является числом.')
         else:
             await message.reply('Неверный формат команды. Команда должна выглядеть так: \"/setup время_проведения_опроса(в часах)\"')
+scheduler.add_job(check_full_done_sheets, 'interval',hours = 1)
+scheduler.add_job(start_polls, 'cron', day = 25, hour = 9)
+scheduler.add_job(notificate_users, 'interval', minutes=1)
+scheduler.start()
 app.run()
