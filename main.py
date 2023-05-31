764c66ff3f1d59dc306193a02a71a260bef4b9ab
mityuk17 <87587511+mityuk17@users.noreply.github.com>
2023-05-31 22:21:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -47,44 +47,7 @@ async def poll_finished():
     for poll in polls:
         if db.check_full_done(poll[0]):
             await make_conclusion(poll[0])
-# async def start_polls():
-#     sh = gc.open_by_url(table_url)
-#     worksheet_list = sh.worksheets()
-#     for sheet in worksheet_list:
-#         if sheet.title == 'example':
-#             continue
-#         worksheet = sh.worksheet(sheet.title)
-#         if worksheet.cell(row=2, col=4).value == 1:
-#             continue
-#         deadline = int(worksheet.cell(row=2,col=2).value)
-#         author_id = worksheet.cell(row=2,col=1).value
-#         chat_id = worksheet.title
-#         chat_name = worksheet.cell(row=2,col=3).value
-#         chat_members = list()
-#         sh.del_worksheet(worksheet)
-#         async for member in app.get_chat_members(int(chat_id)):
-#             chat_members.append([member.user.username, member.user.id])
-#         create_sheet_poll(sheet_name=chat_id, deadline=deadline, author_id=author_id,
-#                           members=chat_members, chat_name=chat_name)
-#         scheduler.add_job(sheet_expired, args=[chat_id], trigger='date',
-#                           run_date=datetime.datetime.now() + datetime.timedelta(hours=int(deadline)))
-#
-# def check_full_done_sheets():
-#     sh = gc.open_by_url(table_url)
-#     worksheet_list = sh.worksheets()
-#     for sheet in worksheet_list:
-#         if sheet.title == 'example':
-#             continue
-#         worksheet = sh.worksheet(sheet.title)
-#         if worksheet.cell(row=2,col=4).value == 0:
-#             continue
-#         users = worksheet.col_values(2)[3:]
-#         flag = True
-#         for user in users:
-#             if get_user_current_question(chat_id=sheet.title, user_id=user):
-#                 flag = False
-#         if flag:
-#             make_conclusion(chat_id = sheet.title)
+
 async def make_conclusion(chat_id):
     response = db.get_response(chat_id=chat_id)
     db.change_status(chat_id=chat_id,status=0)
@@ -109,54 +72,6 @@ async def make_conclusion(chat_id):
     await app.send_photo(chat_id=author_id, photo=f'plots/bar_{chat_id}.png')
     await app.send_document(chat_id=author_id,document=f'responses/{chat_id}.xlsx', caption='Отчёт по опросу в формате .xlsx')
     os.remove(f'plots/{chat_id}.png')
-# def check_existing_sheet(sheet_name):
-#     sh = gc.open_by_url(table_url)
-#     worksheet_list = sh.worksheets()
-#     for i in range(len(worksheet_list)):
-#         worksheet_list[i]= worksheet_list[i].title
-#     return sheet_name in worksheet_list
-# def create_sheet_poll(sheet_name, deadline, author_id, members, chat_name):
-#     sh = gc.open_by_url(table_url)
-#     worksheet = sh.add_worksheet(title=str(sheet_name), rows=100, cols=20)
-#     worksheet.append_row(['id создателя опроса', 'Дедлайн(в часах)', 'Название чата', 'Активный опрос'])
-#     worksheet.append_row([author_id,deadline, chat_name, 1])
-#     worksheet.append_row(['username участника группы','id участника группы'] + [str(i) for i in range(1,11)] + ['У','Удовлетворённость'])
-#     for row in range(len(members)):
-#         worksheet.append_row(members[row] + ['' for i in range(1,11)])
-# def change_poll_status(chat_id,status):
-#     sh = gc.open_by_url(table_url)
-#     worksheet = sh.worksheet(str(chat_id))
-#     worksheet.update_cell(row=2,col=4, value=status)
-# async def write_answer(chat_id,user_id, question_num,answer):
-#     sh = gc.open_by_url(table_url)
-#     worksheet = sh.worksheet(str(chat_id))
-#     cell = worksheet.findall(str(user_id))[-1]
-#     worksheet.update_cell(row = cell.row, col=question_num+2, value=answer)
-# def get_user_current_question(chat_id,user_id):
-#     sh = gc.open_by_url(table_url)
-#     worksheet = sh.worksheet(str(chat_id))
-#     cell = worksheet.findall(str(user_id))[-1]
-#     row_number = cell.row
-#     values_list = worksheet.row_values(row_number)
-#     next_question = None
-#     if len(values_list) < 12:
-#         next_question = len(values_list)-1
-#     return next_question
-# def get_users_for_notification():
-#     users = list()
-#     sh = gc.open_by_url(table_url)
-#     worksheets = sh.worksheets()
-#     for sheet in worksheets:
-#         if sheet.title == 'example':
-#             continue
-#         chat_id = sheet.title
-#         sheet = sh.worksheet(chat_id)
-#         if sheet.cell(row = 2, col=4).value == 1:
-#             continue
-#         for user_id in sheet.col_values(2)[3:]:
-#             if get_user_current_question(chat_id,int(user_id)):
-#                 users.append([chat_id,user_id])
-#     return users
 async def start_polls():
     polls = db.get_inactive_polls()
     for poll in polls:
@@ -202,7 +117,6 @@ async def get_query(client: Client, callback_query: types.CallbackQuery):
         answer_variant =int(callback_query.data.split('=')[1])
         question_num = db.get_user_current_question(chat_id=chat_id, user_id=user_id)
         db.write_answer(chat_id=chat_id,user_id= user_id,question_number=question_num+1, answer=answer_variant)
-        #await app.delete_messages(chat_id=user_id , message_ids=callback_query.message.id)
         if question_num >= 9:
             await app.send_message(chat_id=user_id , text='Вы прошли опрос, спасибо.')
             return True
@@ -228,7 +142,6 @@ async def setup(client : Client, message: types.Message):
                             chat_members.append([member.user.id, member.user.username])
                 db.add_chat(chat_id=message.chat.id,chat_title=message.chat.title,creator_id=message.from_user.id, deadline=int(deadline))
                 db.create_poll_table(chat_id=message.chat.id, members=chat_members)
-                #scheduler.add_job(sheet_expired, args=[message.chat.id], trigger='date', run_date =datetime.datetime.now()+datetime.timedelta(hours=int(deadline)))
                 await message.reply(f'Успешно было запущено проведение опросов в этой группе. Время проведения опросов(в часах): {message.text.split()[-1]}\nЧтобы бот мог отправить вам сообщение с опросом перейдите по ссылке и нажмите кнопку Начать\nhttps://t.me/ContentedStudentBot?start')
             else:
                 await message.reply('Введённый параметр не является числом.')
